dc568c057f4c57905b2b366aa56586f6508300d5
Ajay <dev@ajay.app>
2025-04-11 19:51:54 -0400

    Handle bad json

--- src/routes/getItem.ts
+++ src/routes/getItem.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import DiskCache from "../utils/diskCache";
+import { Logger } from "../utils/logger";
 
 export async function getItem(req: Request, res: Response): Promise<Response> {
     const key = req.query.key as string;
@@ -9,7 +10,12 @@ export async function getItem(req: Request, res: Response): Promise<Response> {
 
     const value = await DiskCache.get(key);
     if (value) {
-        return res.status(200).send(JSON.parse(value as string));
+        try {
+            return res.status(200).send(JSON.parse(value as string));
+        } catch (e) {
+            Logger.error(`Error parsing JSON: ${e}`);
+            return res.status(500).send("Error parsing JSON");
+        }
     } else {
         return res.status(404).send("Not found");
     }
