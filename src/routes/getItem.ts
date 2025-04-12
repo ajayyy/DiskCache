975ffdb3af5f7076ed65fe0d12c968f04c3d31ef
Ajay <dev@ajay.app>
2025-04-11 21:21:25 -0400

    Avoid parsing json

--- src/routes/getItem.ts
+++ src/routes/getItem.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 import DiskCache from "../utils/diskCache";
-import { Logger } from "../utils/logger";
 
 export async function getItem(req: Request, res: Response): Promise<Response> {
     const key = req.query.key as string;
@@ -10,12 +9,9 @@ export async function getItem(req: Request, res: Response): Promise<Response> {
 
     const value = await DiskCache.get(key);
     if (value) {
-        try {
-            return res.status(200).send(JSON.parse(value as string));
-        } catch (e) {
-            Logger.error(`Error parsing JSON: ${e}`);
-            return res.status(500).send("Error parsing JSON");
-        }
+        return res.status(200).header({
+            "Content-Type": "application/json"
+        }).send(value);
     } else {
         return res.status(404).send("Not found");
     }
